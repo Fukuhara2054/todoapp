57fbdb8ae7c751e9822cc4b2410a7a2a57745c2e
Fukuhara2054 <t.fukuhara@g.dreamcareer.co.jp>
2022-04-19 16:11:47 +0900

    更新部分；

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const items = require("../../src/items");
 const tasks = require("../../src/tasks/create.js");
-
+const get = require("../../src/tasks/get.js");
 var router = express.Router();
 
 /* 商品一覧を取得するルーティング */
@@ -12,7 +12,7 @@ router.get("/items", async function (req, res, next) {
 
 /*１件の商品情報を取得するルーティング */
 router.get("/items/:id", function (req, res, next) {
-  const item = items.getItem(req.params.id);
+  const item = get.getItem(req.params.id);
   res.send(item);
 });
 //``新規登録取得のルーティング
