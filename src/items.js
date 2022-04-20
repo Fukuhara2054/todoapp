855ed0fa209c461943002c6c0c1494557c145635
Fukuhara2054 <t.fukuhara@g.dreamcareer.co.jp>
2022-04-20 15:23:20 +0900

    更新のサンプル作成終了；

--- src/items.js
+++ src/items.js
@@ -14,7 +14,7 @@ getListItem = async function () {
     connection = await mysql.createConnection(config.dbSetting);
     //sqlをここに書く
     const sql =
-      " SELECT ` t_task`.id, ` t_task`.category_id, ` m_category`.category_name, ` t_task`.task_name, ` t_task`.deadline, ` t_task`.task_status, ` t_task`.updated_at, ` t_task`.created_at FROM ` t_task` LEFT JOIN ` m_category` ON ` t_task`.category_id = ` m_category`.id;";
+      " SELECT ` t_task`.id, ` t_task`.category_id, ` m_category`.category_name, ` t_task`.task_name, ` t_task`.deadline, ` t_task`.task_status, ` t_task`.updated_at, ` t_task`.created_at FROM ` t_task` LEFT JOIN ` m_category` ON ` t_task`.category_id = ` m_category`.category_id;";
     const [rows, fields] = await connection.query(sql);
 
     return rows;
--- src/tasks/update.js
+++ src/tasks/update.js
@@ -6,14 +6,14 @@ patchid = async function (id, body) {
   let connection = null;
   try {
     connection = await mysql.createConnection(config.dbSetting);
-
+    console.log(body);
     const sql =
       "UPDATE ` t_task` SET ` t_task`.task_name=?, ` t_task`.deadline=?, ` t_task`.category_id=?, ` t_task`.task_status=?, ` t_task`.updated_at=? WHERE ` t_task`.id=?;";
     let d = [
-      body.task_name,
+      body.task,
       body.deadline,
       body.category,
-      body.stutas,
+      body.status,
       new Date(),
       id,
     ];
