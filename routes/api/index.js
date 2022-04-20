758909f59c63ee92e6c9ff6e894e73bdcaa6e7d6
Fukuhara2054 <t.fukuhara@g.dreamcareer.co.jp>
2022-04-20 14:46:53 +0900

    更新；

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 const items = require("../../src/items");
 const tasks = require("../../src/tasks/create.js");
 const get = require("../../src/tasks/get.js");
+const update = require("../../src/tasks/update.js");
 var router = express.Router();
 
 /* 商品一覧を取得するルーティング */
@@ -11,14 +12,19 @@ router.get("/items", async function (req, res, next) {
 });
 
 /*１件の商品情報を取得するルーティング */
-router.get("/items/:id", function (req, res, next) {
-  const item = get.getItem(req.params.id);
+router.get("/get/:id", async function (req, res, next) {
+  const item = await get.getItem(req.params.id);
   res.send(item);
 });
 //``新規登録取得のルーティング
 router.post("/tasks", function (req, res, next) {
   const postTasks = tasks.postTasks(req.body);
   res.send(postTasks);
+});
+//更新のルーティング
+router.patch("/update/:id", async function (req, res, next) {
+  const patchid = await update.patchid(req.params.id, req.body);
+  res.send(patchid);
 });
 
 module.exports = router;
--- src/tasks/update.js
+++ src/tasks/update.js
@@ -1,1 +1,29 @@
 // 更新処理
+const mysql = require("mysql2/promise");
+const config = require("../../config.js");
+
+patchid = async function (id, body) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+
+    const sql =
+      "UPDATE ` t_task` SET ` t_task`.task_name=?, ` t_task`.deadline=?, ` t_task`.category_id=?, ` t_task`.task_status=?, ` t_task`.updated_at=? WHERE ` t_task`.id=?;";
+    let d = [
+      body.task_name,
+      body.deadline,
+      body.category,
+      body.stutas,
+      new Date(),
+      id,
+    ];
+    const [rows, fields] = await connection.query(sql, d);
+    console.log(rows);
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+exports.patchid = patchid;
